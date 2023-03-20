444bfc7648e19d4569917d23270a8f1e7def0360
ziedlahbib <72930782+ziedlahbib@users.noreply.github.com>
2023-03-20 21:41:47 +0100

    ->creditserviceimpl

---
+++ src/main/java/Interface/ICreditservice.java
@@ -0,0 +1,18 @@
+package Interface;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import tn.esprit.spring.entity.Credit;
+
+
+@Service
+public interface ICreditservice {
+	public Credit AjoutCredit(Credit e);
+	public Credit UpdateCredit(Credit e,Long idCredit);
+	public void SupprimerCredit(Long idCredit);
+	public Credit AffichDetailCredit(Long idCredit);
+	public List<Credit> afiichListCredit();
+
+}
---
+++ src/main/java/tn/esprit/spring/service/CreditServiceImpl.java
@@ -0,0 +1,40 @@
+package tn.esprit.spring.service;
+
+import java.util.List;
+
+import Interface.ICreditservice;
+import tn.esprit.spring.entity.Credit;
+
+public class CreditServiceImpl implements ICreditservice {
+
+	@Override
+	public Credit AjoutCredit(Credit e) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Credit UpdateCredit(Credit e, Long idCredit) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void SupprimerCredit(Long idCredit) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Credit AffichDetailCredit(Long idCredit) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Credit> afiichListCredit() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
