71afd3b129d6842faf554116822a10b0b5743139
ziedlahbib <72930782+ziedlahbib@users.noreply.github.com>
2023-03-20 15:00:27 +0100

    ajout repository

---
+++ src/main/java/tn/esprit/spring/repository/ProduitRepository.java
@@ -0,0 +1,14 @@
+package tn.esprit.spring.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import tn.esprit.spring.entity.Produit;
+
+
+
+@Repository
+public interface ProduitRepository extends JpaRepository<Produit, Long>{
+	boolean existsByGeneratedString(String generatedString);
+
+}
