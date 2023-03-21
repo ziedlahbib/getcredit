7b0961072b0f383ec9e75b84a3daf10ccacc740e
ziedlahbib <72930782+ziedlahbib@users.noreply.github.com>
2023-03-21 17:27:37 +0100

    creditservice

--- src/main/java/tn/esprit/spring/service/CreditServiceImpl.java
+++ src/main/java/tn/esprit/spring/service/CreditServiceImpl.java
@@ -2,39 +2,47 @@ package tn.esprit.spring.service;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
 import Interface.ICreditservice;
 import tn.esprit.spring.entity.Credit;
+import tn.esprit.spring.repository.CreditRepository;
 
 public class CreditServiceImpl implements ICreditservice {
 
+	@Autowired
+	CreditRepository creditRepo;
 	@Override
 	public Credit AjoutCredit(Credit e) {
-		// TODO Auto-generated method stub
-		return null;
+		return creditRepo.save(e);
 	}
 
 	@Override
 	public Credit UpdateCredit(Credit e, Long idCredit) {
-		// TODO Auto-generated method stub
-		return null;
+		Credit c=creditRepo.findById(idCredit).orElse(null);
+		c.setDateDebut(e.getDateDebut());
+		c.setDateFin(e.getDateFin());
+		c.setMontant(e.getMontant());
+		c.setMontantparmois(e.getMontantparmois());
+		c.setRestapayer(e.getRestapayer());
+		return creditRepo.save(c);
 	}
 
 	@Override
 	public void SupprimerCredit(Long idCredit) {
-		// TODO Auto-generated method stub
+		creditRepo.deleteById(idCredit);
 		
 	}
 
 	@Override
 	public Credit AffichDetailCredit(Long idCredit) {
-		// TODO Auto-generated method stub
-		return null;
+		
+		return creditRepo.findById(idCredit).orElse(null);
 	}
 
 	@Override
 	public List<Credit> afiichListCredit() {
-		// TODO Auto-generated method stub
-		return null;
+		return creditRepo.findAll();
 	}
 
 }
