e0d169fa1bed9e7c8f023581e335e40e3428b6b8
ziedlahbib <72930782+ziedlahbib@users.noreply.github.com>
2023-05-04 22:14:56 +0100

    ocr controler

---
+++ src/main/java/tn/esprit/spring/controller/OCRController.java
@@ -0,0 +1,34 @@
+package tn.esprit.spring.controller;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import net.sourceforge.tess4j.Tesseract;
+import net.sourceforge.tess4j.TesseractException;
+
+@RestController
+public class OCRController {
+	  private Tesseract tesseract = new Tesseract();
+
+	    @PostMapping("/ocr")
+	    public String ocr(@RequestParam("file") MultipartFile file) throws IOException, TesseractException {
+	        File imageFile = convertMultipartFileToFile(file);
+	        String result = tesseract.doOCR(imageFile);
+	        return result;
+	    }
+
+	    private File convertMultipartFileToFile(MultipartFile file) throws IOException {
+	        File convertedFile = new File(file.getOriginalFilename());
+	        FileOutputStream fos = new FileOutputStream(convertedFile);
+	        fos.write(file.getBytes());
+	        fos.close();
+	        return convertedFile;
+	    }
+
+}
