cc6403e1cabb1662f2faec028bb876bbe40bcbaa
ziedlahbib <72930782+ziedlahbib@users.noreply.github.com>
2023-08-03 01:27:55 +0100

    satt ok

---
+++ src/main/java/tn/esprit/spring/controller/StaticticController.java
@@ -0,0 +1,44 @@
+package tn.esprit.spring.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import tn.esprit.spring.entity.Creditrefuse;
+import tn.esprit.spring.service.StatistiqueService;
+
+@CrossOrigin(origins = "http://localhost:4200/",exposedHeaders="Access-Control-Allow-Origin" )
+@RestController
+@RequestMapping("/statistic")
+public class StaticticController {
+	@Autowired
+	StatistiqueService statserv;
+	@GetMapping("/get-creditref-pourcentage")
+	@ResponseBody
+	public float getcreditrefpourc() {
+		return statserv.pourcentagecrdref();
+
+	}
+	@GetMapping("/get-credit-pourcentage")
+	@ResponseBody
+	public float getcreditpourc() {
+		return statserv.pourcentagecrd();
+
+	}
+	@GetMapping("/get-creditref-ent/{id-ent}")
+	@ResponseBody
+	public float nbrcreditrefparent(@PathVariable("id-ent") Long ident) {
+		return statserv.nbrcreditrefparent(ident);
+
+	}
+	@GetMapping("/get-credit-ent/{id-ent}")
+	@ResponseBody
+	public float nbrcreditparent(@PathVariable("id-ent") Long ident) {
+		return statserv.nbrcreditparent(ident);
+
+	}
+}
---
+++ src/main/java/tn/esprit/spring/service/StatistiqueService.java
@@ -0,0 +1,66 @@
+package tn.esprit.spring.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import lombok.extern.slf4j.Slf4j;
+import tn.esprit.spring.entity.Credit;
+import tn.esprit.spring.entity.Creditrefuse;
+import tn.esprit.spring.repository.CreditRefuseRepository;
+import tn.esprit.spring.repository.CreditRepository;
+
+@Slf4j
+
+@Service
+public class StatistiqueService {
+
+	@Autowired
+	CreditRefuseRepository creditRefuseRepo;
+	@Autowired
+	CreditRepository creditRepo;
+	public float pourcentagecrdref() {
+		return  creditRefuseRepo.findAll().size();
+//		int cr=creditRepo.findAll().size();	
+//		log.info("p"+(crf*100)/(crf+cr));
+//		return (crf*100)/(crf+cr);
+		
+	}
+	public float pourcentagecrd() {
+//		int crf= creditRefuseRepo.findAll().size();
+//		int cr=creditRepo.findAll().size();	
+//		log.info("p"+(cr*100)/(crf+cr));
+//		return (cr*100)/(crf+cr);
+		return creditRepo.findAll().size();
+		
+	}
+	public float nbrcreditrefparent(Long ident) {
+		List<Creditrefuse> crf= creditRefuseRepo.findAll();
+		int n=0;
+		for(Creditrefuse cf:crf) {
+				if(cf.getAgentr().getMagasin().getEntreprise().getEntrpriseId()==ident) {
+					n++;
+					log.info("nbrcrditent"+n);
+				}
+			
+			
+		}
+		return n;
+		
+	}
+	public float nbrcreditparent(Long ident) {
+		List<Credit> c= creditRepo.findAll();
+		int n=0;
+		for(Credit cr:c) {
+				if(cr.getAgent().getMagasin().getEntreprise().getEntrpriseId()==ident) {
+					n++;
+					log.info("nbrcrditent"+n);
+				}
+			
+			
+		}
+		return n;
+		
+	}
+}
